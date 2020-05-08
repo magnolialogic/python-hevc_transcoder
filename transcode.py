b3632085205fe62b6f102aa9a67bdfc078171208
Chris Coffin <me@chriscoffin.org>
2020-05-07 21:35:39 -0400

    Smarter and prettier display output, refactored logging

--- transcode.py
+++ transcode.py
@@ -12,21 +12,17 @@ import sys
 
 class Session():
 	class Settings:
-		class Low:
-			RF = 21 # 18-22
-			ENCOPTS = "ctu=32:qg-size:16"
+		class RF:
+			SD = 21
+			HD = 22
+			FHD = 23
+			UHD = 26
 		
-		class Medium:
-			RF = 22 # 19-23
-			ENCOPTS = "ctu=32:qg-size:32"
-		
-		class High:
-			RF = 23 # 20-24
-			ENCOPTS = "ctu=64:qg-size:64"
-		
-		class Ultra:
-			RF = 26 # 22-28
-			ENCOPTS = "ctu=64:qg-size:64"
+		class ENCOPTS:
+			SD = "ctu=32:qg-size=16"
+			HD = "ctu=32:qg-size=32"
+			FHD = "ctu=64:qg-size=64"
+			ENCOPTS = "ctu=64:qg-size=64"
 	
 	def __init__(self, file):
 		signal.signal(signal.SIGINT, self.signal_handler)
@@ -37,94 +33,101 @@ class Session():
 		metadata = json.loads(metadata)["streams"][0]
 		
 		# Populate metadata-based attributes
-		self.source_path = file
-		self.height = int(metadata["height"])
-		self.width = int(metadata["width"])
-		self.duration = metadata["duration"]
-		self.filename = os.path.splitext(os.path.relpath(self.source_path, "source"))[0]
-		self.filesize = os.path.getsize(self.source_path)
-		self.bitrate = metadata["bit_rate"]
-		self.codec = metadata["codec_name"]
+		self.paths = {"source": file}
+		self.source = {"height": int(metadata["height"]), "width": int(metadata["width"]), "duration": float(metadata["duration"]), "filename": os.path.splitext(os.path.relpath(self.paths["source"], "source"))[0], "filesize": os.path.getsize(self.paths["source"]), "bitrate": int(metadata["bit_rate"]), "frames": int(metadata["nb_frames"]), "codec": metadata["codec_name"]}
+		height = self.source["height"]
+		if height < 720:
+			resolution = "SD"
+		elif 720 <= height < 1080:
+			resolution = "HD"
+		elif 1080 <= height < 2160:
+			resolution = "FHD"
+		elif 2160 <= height:
+			resolution = "UHD"
+		self.source["resolution"] = resolution
 		
 		# Create empty attributes for dynamic session options
-		self.quality = None
-		self.preset = None
+		self.encoder_quality = None
+		self.encoder_preset = None
 		self.preset_name = None
-		self.encopts = None
+		self.encoder_options = None
 		
 		# Construct session options and parameters
 		self.map_options()
-		self.output_filename_decorator = "_RF" + str(self.quality)
+		self.file_decorator = "_RF" + str(self.encoder_quality)
 		if args.best:
-			self.output_filename_decorator += "_Best"
+			self.file_decorator += "_Best"
 		elif args.baseline:
-			self.output_filename_decorator += "_Baseline"
+			self.file_decorator += "_Baseline"
 		if args.small:
-			self.output_filename_decorator += "_Small"
-		self.output_path = "hevc/" + self.filename + self.output_filename_decorator + ".mp4"
-		self.log_path = "performance/" + self.filename + self.output_filename_decorator + ".log"
+			self.file_decorator += "_Small"
+		if args.preset:
+			self.file_decorator += "_{preset}".format(preset=args.preset.capitalize())
+		self.paths["output"] = "hevc/" + self.source["filename"] + self.file_decorator + ".mp4"
+		self.paths["log"] = "performance/" + self.source["filename"] + self.file_decorator + ".log"
 		
 		# Build HandBrakeCLI command
-		self.arguments = "HandBrakeCLI --encoder-preset {video_preset} --preset-import-file presets.json --preset {preset_name} --quality {quality} --encopts {encopts} --input {source_path} --output {output_path}".format(video_preset=self.preset, preset_name=self.preset_name, quality=str(self.quality), encopts=self.encopts, source_path=self.source_path, output_path=self.output_path)
+		self.args = "HandBrakeCLI --encoder-preset {encoder_preset} --preset-import-file presets.json --preset {preset_name} --quality {quality} --encopts {encopts} --input {source_path} --output {output_path}".format(encoder_preset=self.encoder_preset, preset_name=self.preset_name, quality=str(self.encoder_quality), encopts=self.encoder_options, source_path=self.paths["source"], output_path=self.paths["output"])
 		
 		# Validate and finish
 		self.validate()
 		self.summarize()
 	
 	def validate(self):
+		"""	Verifies that no session attributes are null
+		"""
 		if any(value is None for attribute, value in self.__dict__.items()):
-			sys.exit("FATAL: Session.validate(): found null attribute for " + self.source_path)
+			sys.exit("FATAL: Session.validate(): found null attribute for " + self.paths["source"])
 	
 	def summarize(self):
-		print()
-		print(self.source_path)
+		"""	Summarize transcode session before starting
+		"""
+		print("{date}: {source}:".format(date=str(datetime.now()), source=self.paths["source"]))
 		pprint(vars(self))
 		print()
 	
 	def map_options(self):
-		# Start with settings based on source resolution
-		if self.height < 720:
-			self.quality = self.Settings.Low.RF
-			self.encopts = self.Settings.Low.ENCOPTS
-		elif 720 <= self.height < 1080:
-			self.quality = self.Settings.Medium.RF
-			self.encopts = self.Settings.Medium.ENCOPTS
-		elif 1080 <= self.height < 2160:
-			self.quality = self.Settings.High.RF
-			self.encopts = self.Settings.High.ENCOPTS
-		elif 2160 <= self.height:
-			self.quality = self.Settings.Ultra.RF
-			self.encopts = self.Settings.Ultra.ENCOPTS
-		
-		# Override defaults based on command-line arguments
+		"""	Start with settings based on source resolution and then override defaults based on command-line arguments
+		"""
+		self.encoder_quality = getattr(self.Settings.RF, self.source["resolution"])
+		self.encoder_options = getattr(self.Settings.ENCOPTS, self.source["resolution"])
 		if args.best:
 			self.preset_name = "Best"
 		elif args.baseline:
 			self.preset_name = "Baseline"
 		else:
 			self.preset_name = "Default"
-		
 		if args.preset:
-			self.preset = args.preset.lower()
+			self.encoder_preset = args.preset.lower()
 		else:
-			self.preset = "slow"
-		
+			self.encoder_preset = "slow"
 		if args.quality:
-			self.quality = args.quality
-		
+			self.encoder_quality = args.quality
 		if args.small:
-			self.encopts += ":tu-intra-depth=3:tu-inter-depth=3"
+			self.encoder_options += ":tu-intra-depth=3:tu-inter-depth=3"
 	
 	def signal_handler(self, sig, frame):
+		"""	Delete output file if ctrl+c is caught, since file will be corrupt
+		"""
 		self.cleanup()
-		sys.exit("Caught ^c: exiting.")
+	
+	def log(self, elapsed_time, fps):
+		"""	Summarizes transcode session for screen and log
+		"""
+		with open(self.paths["log"], "w") as logfile:
+			summary = str(elapsed_time) + "\n" + str(fps) + " fps"
+			logfile.write(summary + "\n\n" + session.args + "\n\n")
+			pprint(vars(self), logfile)
+			print(summary)
 	
 	def cleanup(self):
+		"""	Always deletes output file, deletes log if --delete is passed from command-line
+		"""
+		if os.path.exists(self.paths["output"]):
+				os.remove(self.paths["output"])
 		if args.delete:
-			if os.path.exists(self.output_path):
-				os.remove(self.output_path)
-			if os.path.exists(self.log_path):
-				os.remove(self.log_path)
+			if os.path.exists(self.paths["log"]):
+				os.remove(self.paths["log"])
 
 # Define command-line arguments
 parser = argparse.ArgumentParser()
@@ -149,7 +152,7 @@ elif args.file and not os.path.exists(args.file):
 elif args.preset and not args.preset.lower() in ("ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow", "placebo"):
 	print("FATAL:", args.preset, "not valid!")
 elif args.quality and not args.quality in range(-12, 51):
-	print("FATAL: quality must be between -12 and 51 (lower is slower/higher quality)")
+	print("FATAL: quality must be between -12 and 51 (lower is slower + higher quality)")
 else:
 	valid_arguments = True
 if not valid_arguments:
@@ -163,39 +166,39 @@ elif args.all and args.quality:
 		if reply[0] == "n":
 			sys.exit("Aborting invocation with --all and --quality options.")
 
-# Build list of source files
+# Build list of source files and create directories if necessary
 extensions = [".mp4", ".m4v", ".mov", ".mkv", ".mpg", ".mpeg", ".avi", ".wmv", ".flv", ".webm", ".ts"]
 if args.all:
-	source_files = ["source/" + file for file in os.listdir("source") if file[file.rindex("."):].lower() in extensions]
+	source_files = ["source/" + file for file in os.listdir("source") if os.path.splitext(file)[1].lower() in extensions]
 else:
 	source_files = [args.file]
-
-# Do the thing
 if not os.path.exists("performance"):
 	os.mkdir("performance")
 if not os.path.exists("hevc"):
 	os.mkdir("hevc")
+
+# Do the thing
+start_time = datetime.now()
+print("\n{date}: Starting transcode session for {source_files}\n".format(date=str(datetime.now()), source_files=str(source_files)))
 for file in source_files:
 	session = Session(file)
-	if not os.path.exists(session.output_path):
-		with open(session.log_path, "w") as log:
-			sys.stdout.write(session.arguments)
-			log.write(session.arguments)
-			sys.stdout.write("")
-			log.write("")
-			start_time = datetime.now()
-			with subprocess.Popen(shlex.split(session.arguments, posix=False), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1) as process:
-				for line in process.stdout:
-					sys.stdout.write(line)
-					log.write(line)
-			end_time = datetime.now()
-			elapsed_time = end_time - start_time
-			sys.stdout.write(str(elapsed_time))
-			log.write(str(elapsed_time))
+	if not os.path.exists(session.paths["output"]):
+		print(session.args + "\n")
+		session_start_time = datetime.now()
+		transcode = subprocess.Popen(shlex.split(session.args, posix=False))
+		transcode.wait()
+		session_end_time = datetime.now()
+		session_elapsed_time = session_end_time - session_start_time
+		fps = session.source["frames"] / session_elapsed_time.seconds
+		print("\n{date}: Finished {output_file}".format(date=str(session_end_time), output_file=session.paths["output"]))
+		session.log(session_elapsed_time, fps)
 	else:
-		print(session.output_path, "already exists, skipping.")
+		print(session.paths["output"], "already exists, skipping.")
 	if args.delete:
 		session.cleanup()
-	print()
+	print("\n\n\n\n\n")
 
-sys.exit("{date}: Done.\n".format(date=str(datetime.now())))
+end_time = datetime.now()
+elapsed_time = end_time - start_time
+
+sys.exit("{date}: Finished after {elapsed_time}.\n".format(date=str(datetime.now()), elapsed_time=elapsed_time))
